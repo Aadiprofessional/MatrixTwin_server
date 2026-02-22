f474eaf12e7da0a314db71f6d147551dcad2c862
Aadiprofessional <aadiprofessional8@gmail.com>
2026-02-22 19:20:53 +0530

    cros issue

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const app = express();
 app.use(cors({
   origin: true, // Reflects the request origin
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin', 'apikey', 'x-client-info'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin', 'apikey', 'x-client-info', 'x-auth-token'],
   credentials: true
 }));
 
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
 
   res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin, apikey, x-client-info');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin, apikey, x-client-info, x-auth-token');
   res.header('Access-Control-Allow-Credentials', 'true');
   
   if (req.method === 'OPTIONS') {
